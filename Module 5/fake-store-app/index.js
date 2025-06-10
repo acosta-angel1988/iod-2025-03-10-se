1aa0fce56e3ffa1c13861dc6ceaf6422fd2527b7
effilin <effienummer@gmail.com>
2025-06-10 18:51:01 -0400

    if you can get the routes/ controllers running on lab 5 I can give you back 2 points

--- Module 5/fake-store-app/index.js
+++ Module 5/fake-store-app/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const productRoutes = require('./routes/products');
+const express = require("express");
+const cors = require("cors");
+const productRoutes = require("./routes/products");
 
 const app = express();
 const PORT = 3000;
@@ -8,11 +8,12 @@ const PORT = 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/", express.static("public"));
 
 // Routes
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
